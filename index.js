ef12f3641536da3ced81ee803c0616042d934a90
uncletammy <uncletammy@gmail.com>
2014-05-30 10:39:19 -0500

    first commit yay

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = function irc(sails) {
+
+    return {
+        signIn: function(message){
+            var hook = this;
+
+            console.log('Registering Bots!')
+        },
+        // Run automatically when the hook initializes
+        initialize: function (cb) {
+
+            var hook = this;
+            var eventsToWaitFor = [];
+            if (sails.hooks.orm) {
+                eventsToWaitFor.push('hook:orm:loaded');
+            }
+
+            sails.after(eventsToWaitFor, hook.signIn);
+
+
+            // You must trigger `cb` so sails can continue loading.  If you pass in an error, sails will fail to load, and display your error on the console.
+            return cb();
+        },
+    }
+};
